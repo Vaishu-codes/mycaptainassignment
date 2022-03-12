9f6553d69f1178993ee1058bb84052531f25a92e
Vaishu-codes <97692276+Vaishu-codes@users.noreply.github.com>
2022-03-12 19:50:44 +0530

    Delete file_extensions.py

--- file_extensions.py
+++
@@ -1,8 +0,0 @@
-#using the concept of strings-find inbuilt function(but works only for python extensions)
-a = str(input("Enter the file name: "))
-b = a.find(".py")
-print(b)
-if b == ".py":
-    print("The extension used is python")
-else:
-    print("invalid input")
