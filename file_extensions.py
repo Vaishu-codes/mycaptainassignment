667e820e20eb21084498b11fb96f36f6b216e4b8
Vaishu-codes <97692276+Vaishu-codes@users.noreply.github.com>
2022-03-12 19:51:43 +0530

    Add files via upload
    works on os library and gives the extension of the file

---
+++ file_extensions.py
@@ -0,0 +1,10 @@
+import os
+
+split_tup = os.path.splitext('my_file.py')
+print(split_tup)
+  
+file_name = split_tup[0]
+file_extension = split_tup[1]
+  
+print("File Name: ", file_name)
+print("File Extension: ", file_extension)
